95f08903c36af1d4215e852f5b863ecc82159846
jurifauser1395 <139368246+jurifauser1395@users.noreply.github.com>
2023-11-24 19:27:35 +0100

    add advertisement colms

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // Vue instance for handling form data
-var app = new Vue({
+/*var app = new Vue({
     el: '#orderForm',
     data: {
         formData: {
@@ -21,4 +21,24 @@ var app = new Vue({
             $('#orderSummaryModal').modal('show');
         }
     }
-});
+});*/
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
